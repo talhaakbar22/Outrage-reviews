60370a52d6505ac7c02ed0ab16a2c0ba95cb1d80
talha_dev <talhaakbartws@gmail.com>
2026-09-02 11:53:55 +0500

    Update allowed Origins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "16.171.140.220",
+    // add your domain if you use one too, e.g.:
+    // "reviews.yourdomain.com",
+  ],
 };
 
 export default nextConfig;
